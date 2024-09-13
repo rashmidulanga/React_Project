2058723502c1f8e497ec579662b90ebe18a2c2c3
rashmidulanga99 <rashmi.d@sierra.lk>
2024-09-13 13:13:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Signup from "./Component/Signup/Signup";
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Component/Login/Login";
+import Todo from "./Component/Todo/Todo";
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <Signup />,
+    },
+    {
+      path: "/Login",
+      element: <Login />,
+    },
+    {
+      path: "/Todo",
+      element: <Todo />,
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
